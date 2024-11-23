2832bd0ce2e308e6553f5481edde586ccc7227a0
Erbit <erbitservices@gmail.com>
2024-11-23 15:26:36 +0530

    mihir

--- src/assets/Pages/BookData.jsx
+++ src/assets/Pages/BookData.jsx
@@ -47,42 +47,43 @@ function BookData() {
               <table ref={xls}>
                 <thead>
                   <tr>
-                   
+                   <th>Sr no</th>
                     <th> ISBN</th>
                     <th>Book Name</th>
-                    <th>Book Name Guj</th>
-                    <th> Category</th>
-                    <th> Language</th>
+                    <th> AuthorName</th>
                     <th> PublisherName</th>
+                    <th> Price</th>
+                    <th>Pages</th>
                     <th> Size</th>
-                    <th> Subject</th>
                     <th> Weight</th>
                     <th> Binding</th>
-                    <th> AuthorName</th>
-                    <th> AuthorNameGuj</th>
-                    <th> Discribption</th>
+                    <th> Language</th>
+                    <th> Category</th>
+                    <th> Subject</th>
                   </tr>
                 </thead>
                 <tbody>
                   {booklist &&
-                    booklist.map((i) => (
+                    booklist.map((i , index) => (
                       <tr>
                         
                         
+                        <td key={i.index}> {index}</td>
                         <td key={i.ISBN}> {i.ISBN}</td>
                         <td key={i.BookName}> {i.BookName}</td>
-
-                        <td key={i.BookNameGuj}> {i.BookNameGuj}</td>
-                        <td key={i.Category}> {i.Category}</td>
-                        <td key={i.Language}> {i.Language}</td>
+                        <td key={i.AuthorName}> {i.AuthorName}</td>
                         <td key={i.PublisherName}> {i.PublisherName}</td>
+                        <td key={i.Price}> {i.Price}</td>
+                        <td key={i.BookPages
+}> {i.BookPages
+}</td>
                         <td key={i.Size}> {i.Size}</td>
-                        <td key={i.Subject}> {i.Subject}</td>
                         <td key={i.Weight}> {i.Weight}</td>
                         <td key={i.Binding}> {i.Binding}</td>
-                        <td key={i.AuthorName}> {i.AuthorName}</td>
-                        <td key={i.AuthorNameGuj}> {i.AuthorNameGuj}</td>
-                        <td key={i.Discribption}> {i.Discribption}</td>
+                        <td key={i.Language}> {i.Language}</td>
+                        <td key={i.Subject}> {i.Subject}</td>
+                        <td key={i.Category}> {i.Category}</td>
+                        
                       </tr>
                     ))}
                 </tbody>
